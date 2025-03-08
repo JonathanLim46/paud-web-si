e946bfea88f7b55abae8ed61af4583e48bfac793
“mawar” <jonathanfrizy12@gmail.com>
2025-03-09 00:13:53 +0700

    Jonathan Lim/feat: add guru model only for level guru

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'username' => fake()->userName(),
-            'level' => fake()->randomElement(['admin', 'guru']),
+            'level' => 'admin',
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
@@ -34,6 +34,11 @@ class UserFactory extends Factory
         ];
     }
 
+    public function guru(){
+        return $this->state(fn (array $attributes) => [
+            'level' => 'guru',
+        ]);
+    }
     /**
      * Indicate that the model's email address should be unverified.
      */
