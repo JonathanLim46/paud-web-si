be3246542f950f95532d045f58d61f18f60a5cbd
“mawar” <jonathanfrizy12@gmail.com>
2025-04-18 09:55:39 +0700

    Jonathan Lim/feat: add edit profile

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'image' => '/images/user-data/sampleIMG.jpg',
             'remember_token' => Str::random(10),
         ];
     }
