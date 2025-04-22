5b8b8de20097c2d09cf3d0b9ffbd09713a09cc93
“mawar” <jonathanfrizy12@gmail.com>
2025-04-22 12:02:52 +0700

    Jonathan Lim/feat: add dashboard guru

--- app/Livewire/Dashboard/Index.php
+++ app/Livewire/Dashboard/Index.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Dashboard;
 
 use App\Models\Guru;
+use App\Models\Hari;
 use Livewire\Component;
 use App\Models\Pendaftar;
 use App\Models\JadwalPelajaran;
+use Illuminate\Support\Facades\DB;
 
 class Index extends Component
 {
@@ -13,6 +15,10 @@ class Index extends Component
     
     public function render()
     {
+        $urutanHari = [
+            'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'
+        ];
+
         return view('livewire.dashboard.index')->with([
             'murids' => Pendaftar::where('diterima', true)->count(),
             'gurus' => Guru::count(),
